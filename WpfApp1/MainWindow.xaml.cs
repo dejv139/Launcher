13c42d51dfe202635afd7b7647562f8d5d0dc1a4
motuzda16 <motuzda16@DST20706>
2018-12-06 14:00:33 +0100

    a

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            foreach (string dirPath in Directory.GetFiles("D:\\motuzda16", "*.txt",SearchOption.AllDirectories))
+            {
+                string[] s = dirPath.Split('\\');
+                Button newBtn = new Button();
+                newBtn.Click += Button_Click;
+                newBtn.MinHeight = 180;
+                newBtn.Width = 180;
+                newBtn.Content = s[s.Length - 1];
+                files.Items.Add(newBtn);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process proc = new Process();
+            Button btn = (Button)sender;
+            string keyword = btn.Content.ToString();
+            foreach (string dirPath in Directory.GetFiles("D:\\motuzda16", "*.txt", SearchOption.AllDirectories))
+            {
+                string[] s = dirPath.Split('\\');
+                if (s[s.Length - 1] == keyword)
+                {
+                    proc.StartInfo = new ProcessStartInfo(dirPath);
+                    proc.Start();
+                }
+            }
+
+        }
+    }
+}
